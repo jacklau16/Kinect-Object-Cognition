b59e065253c539d3e016462007e3f8fb5c919ebe
Jack Lau <jack.lau@student.reading.ac.uk>
2021-03-29 03:24:05 +0100

    Very preliminary object detection version

---
+++ objdetection.py
@@ -0,0 +1,184 @@
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+
+class ObjDetection:
+
+    def __init__(self):
+        self.W_train = []
+        self.U_train = []
+        self.imgMean_train = []
+        self.objName_train = []
+        self.grid_size = 30
+
+
+    def slice_image_to_imagettes(self, imgInput):
+        grid_size = self.grid_size
+        train_set = []
+        train_row = int(imgInput.shape[0] / grid_size)
+        train_col = int(imgInput.shape[1] / grid_size)
+
+        for row in range(train_row):
+            for col in range(train_col):
+                grid_img = imgInput[row * grid_size:row * grid_size + grid_size, col * grid_size:col * grid_size + grid_size]
+                #plt.imshow(grid_img, 'gray')
+                #plt.show()
+                #train_set[:,row*train_row+col] = grid_img.reshape(grid_size*grid_size)
+                train_set.append(grid_img.flatten())
+
+        return np.array(train_set).T
+
+    def read_and_trunc_img(self, imgFile):
+        grid_size = self.grid_size
+        thresVal = 75
+        img = cv2.imread(imgFile, 0)
+        # imgGrey = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        ret, threshold = cv2.threshold(img, thresVal, 255, cv2.THRESH_BINARY_INV)
+        x, y, w, h = cv2.boundingRect(threshold)
+        #plt.imshow(threshold[y:y + h, x:x + w], 'gray')
+        #plt.show()
+        if (h >= grid_size) & (w >= grid_size):
+            imgTrain = threshold[y:y + h, x:x + w]
+            #plt.imshow(imgTrain, 'gray')
+            #plt.show()
+            #print("h,w,grid_size=", h, w, grid_size)
+            return imgTrain
+        else:
+            return None
+
+
+    def train(self, imgFile, labelTrain):
+
+        eigen_vector_used = 10
+
+        imgTrain = self.read_and_trunc_img(imgFile)
+
+        if imgTrain is not None:
+            train_set = self.slice_image_to_imagettes(imgTrain)
+
+            # Compute average image
+            imgMean = np.mean(train_set, axis=1)
+
+            # Normalise images by subtracting with their mean
+            X = train_set - np.tile(imgMean, (train_set.shape[1], 1)).T
+
+            # Perform SVD and obtain eigenvectors
+            U, sigma, VT = np.linalg.svd(X, full_matrices=False)
+
+            # Project the train set onto the first N principal components (eigenvectors)
+            U_sub = U[:,0:eigen_vector_used]
+
+            W = U_sub.T @ X
+        #    W_trained = W
+        else:
+            print(labelTrain,"imgTrain is None")
+            W = None
+            U_sub = None
+            imgMean = None
+
+        return W, U_sub, imgMean
+
+    def recognise_imagette(self, imgInput, imgMean, W, U):
+
+        imgDistThreshold = 10000
+        weightDistThreshold = 100
+
+        X_input = imgInput - imgMean
+        W_input = U.T @ X_input
+        imgRecon = U @ W_input
+        imgRecon = imgRecon + imgMean
+
+        # Compute the Euclidean distance of two matrices using matrix norm
+        dist = np.linalg.norm(imgInput - imgRecon)
+
+        #if dist > imgDistThreshold:
+        #    print("Reconstructed Image has too large distance from original:", dist)
+     #   print("Image Distance: ", dist)
+
+        matchedCount = 0
+        # Compute Euclidean distance of the input image weight against all others
+        # e_k = || W_k - W_r ||
+        for i in range(W.shape[1]):
+            e_k = np.linalg.norm(W_input - W[:, i])
+            if e_k < weightDistThreshold:
+     #           print(f'{i}: Euclidean Distance of Weights = {e_k}')
+                matchedCount += 1
+
+        if matchedCount > 0:
+            return True
+        else:
+            return False
+
+    def recognise_imagette2(self, imgInput, index):
+
+        imgDistThreshold = 10000
+        weightDistThreshold = 100
+
+        #print("imgInput:", imgInput.shape)
+        #print("imgMean:", self.imgMean_train)
+        X_input = imgInput - self.imgMean_train[index]
+        W_input = self.U_train[index].T @ X_input
+        imgRecon = self.U_train[index] @ W_input
+        imgRecon = imgRecon + self.imgMean_train[index]
+
+        # Compute the Euclidean distance of two matrices using matrix norm
+        dist = np.linalg.norm(imgInput - imgRecon)
+
+        #if dist > imgDistThreshold:
+        #    print("Reconstructed Image has too large distance from original:", dist)
+     #   print("Image Distance: ", dist)
+
+        matchedCount = 0
+        # Compute Euclidean distance of the input image weight against all others
+        # e_k = || W_k - W_r ||
+        for i in range(self.W_train[index].shape[1]):
+            e_k = np.linalg.norm(W_input - self.W_train[index][:, i])
+            if e_k < weightDistThreshold:
+     #           print(f'{i}: Euclidean Distance of Weights = {e_k}')
+                matchedCount += 1
+
+        if matchedCount > 0:
+            return True
+        else:
+            return False
+
+
+    def recognise(self, imgInput, index):
+        matchedCount = 0
+        imagettes = self.slice_image_to_imagettes(imgInput)
+
+        for i in range(imagettes.shape[1]):
+            #print(f'Recognising # {i}...')
+            if self.recognise_imagette(imagettes[:,i], self.imgMean_train[index], self.W_train[index], self.U_train[index]):
+                matchedCount += 1
+
+        return matchedCount
+        #print(f'{matchedCount} feature(s) matched.')
+
+
+    def test_run(self):
+        # Giving path for training images
+        trainPath = './train/'
+        # Readling all the images and storing into an array
+        trainImgFiles = os.listdir(trainPath)
+
+        for trainImgFile in trainImgFiles:
+            objName = os.path.splitext(trainImgFile)[0]
+            W, U, imgMean = self.train(f'{trainPath}/{trainImgFile}', objName)
+            self.W_train.append(W)
+            self.U_train.append(U)
+            self.imgMean_train.append(imgMean)
+            self.objName_train.append(objName)
+
+        testIdx = 0
+        imgTest = self.read_and_trunc_img(f'{trainPath}/{trainImgFiles[testIdx]}')
+        print("Test Object:", self.objName_train[testIdx])
+
+        for i in range(len(self.objName_train)):
+            print("Recognising", self.objName_train[i],'...')
+            self.recognise(imgTest, i)
+
+
+    #if __name__ == "__main__":
+    #    exit(main())
---
+++ test_run.py
@@ -0,0 +1,89 @@
+#!/usr/bin/env python
+
+from argparse import ArgumentParser
+from FreenectPlaybackWrapper.PlaybackWrapper import FreenectPlaybackWrapper
+
+import cv2
+
+import objdetection
+
+def main():
+
+    # load and train first
+    detector = objdetection.ObjDetection()
+    detector.test_run()
+
+    parser = ArgumentParser(description="OpenCV Demo for Kinect Coursework")
+    parser.add_argument("videofolder", help="Folder containing Kinect video. Folder must contain INDEX.txt.",
+                        default="ExampleVideo", nargs="?")
+    #parser.add_argument("--no-realtime", action="store_true", default=False)
+    parser.add_argument("--no-realtime", action="store_true", default=True)
+    args = parser.parse_args()
+
+    count = 0
+    classcount = 0
+    lastObjCount = 0
+    newClassStarted = False
+
+    thresVal = 80
+    x_offset = -40
+    y_offset = 30
+
+    labelFile = open('Set1Labels.txt', 'r')
+    classLabels = labelFile.read().splitlines()
+    w =0
+    h =0
+
+    for status, rgb, depth in FreenectPlaybackWrapper(args.videofolder, not args.no_realtime):
+        count += 1
+        # If we have an updated RGB image, then display
+        if status.updated_rgb:
+            if (h > 0) & (w > 0):
+                cv2.rectangle(rgb, (x+x_offset, y+y_offset), (x+x_offset+w, y+y_offset+h), (0, 0, 255))
+            cv2.imshow("RGB", rgb)
+            #print(f"{count}: RGB")
+        # If we have an updated Depth image, then display
+        if status.updated_depth:
+            #lastObjCount = count
+            #cv2.imshow("Depth", depth)
+            #print(f"{count}: DEPTH")
+            ret, thresholded = cv2.threshold(depth, thresVal, 255, cv2.THRESH_BINARY_INV)
+            x, y, w, h = cv2.boundingRect(thresholded)
+            #print(x, y, w, h)
+            if (h>0) & (w>0):
+                cv2.putText(depth, classLabels[classcount], (50,50), cv2.FONT_HERSHEY_PLAIN, 2, (255,0,0), 2)
+    #            cv2.rectangle(depth, (x,y), (x+w, y+h), (0,0,255))
+                cv2.imshow("Depth", depth)
+                lastObjCount = count
+                newClassStarted = True
+
+                # Test Run Object Recognition
+
+                if (h >= detector.grid_size) & (w >= detector.grid_size):
+                    max_score = -1
+                    max_idx = -1
+                    for i in range(len(detector.objName_train)):
+                        score = detector.recognise(thresholded[y:y + h, x:x + w], i)
+                        #print(detector.objName_train[i], score)
+                        if score > max_score:
+                            max_score = score
+                            max_idx = i
+
+                    print("Recognition: ", detector.objName_train[max_idx], score)
+
+        if (count - lastObjCount > 80) & newClassStarted:
+            classcount += 1
+            newClassStarted = False
+
+        #print(count, lastObjCount, newClassStarted)
+        # Check for Keyboard input.
+        key = cv2.waitKey(10)
+
+        # Break out of the program if ESC is pressed (OpenCV KeyCode 27 is ESC key)
+        if key == 27:
+            break
+
+    return 0
+
+if __name__ == "__main__":
+    exit(main())
